adf0ba332dea4da5c8f22cec2386da031e13de59
Henry Reaño <henry29.2004@hotmail.com>
2025-05-03 16:28:22 -0500

    chore(swr18): add styles to learn more button in plans component

--- src/app/landing-page/components/plan-item/plan-item.component.ts
+++ src/app/landing-page/components/plan-item/plan-item.component.ts
@@ -1,12 +1,13 @@
 import {Component, Input} from '@angular/core';
-import {MatButton} from '@angular/material/button';
+import {MatAnchor, MatButton} from '@angular/material/button';
 import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-plan-item',
   imports: [
     MatButton,
-    TranslatePipe
+    TranslatePipe,
+    MatAnchor
   ],
   templateUrl: './plan-item.component.html',
   styleUrl: './plan-item.component.css'
