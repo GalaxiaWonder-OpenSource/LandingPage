20cdc06a1cea69dff10c2cdaa6959ecbd2e8d86d
Henry Reaño <henry29.2004@hotmail.com>
2025-05-03 22:24:11 -0500

    feat(swr12): add client testimonial in testimonial model

--- src/app/landing-page/model/testimonial-card.ts
+++ src/app/landing-page/model/testimonial-card.ts
@@ -7,6 +7,7 @@ export interface TestimonialCard {
 export function getTestimonials(): TestimonialCard[] {
     return [
       { photo: 'testimonials-first.webp', fullname: 'testimonials.first.name', content: 'testimonials.first.content' },
-      { photo: 'testimonials-second.jpg', fullname: 'testimonials.second.name', content: 'testimonials.second.content' }
+      { photo: 'testimonials-second.jpg', fullname: 'testimonials.second.name', content: 'testimonials.second.content' },
+      { photo: 'testimonials-third.jpg', fullname: 'testimonials.third.name', content: 'testimonials.third.content' }
     ];
 }
