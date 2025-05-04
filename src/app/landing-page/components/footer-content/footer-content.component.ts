126b04364fbb8d4346de66d458d697a72cd79398
Henry Reaño <henry29.2004@hotmail.com>
2025-05-03 23:00:37 -0500

    feat(swr17): add terms and conditions component

--- src/app/landing-page/components/footer-content/footer-content.component.ts
+++ src/app/landing-page/components/footer-content/footer-content.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {MatGridList, MatGridTile} from '@angular/material/grid-list';
 import {MatIconModule} from '@angular/material/icon';
 import {TranslatePipe} from '@ngx-translate/core';
+import {TermsAndConditionsComponent} from '../terms-and-conditions/terms-and-conditions.component';
 
 @Component({
   selector: 'app-footer-content',
@@ -9,7 +10,8 @@ import {TranslatePipe} from '@ngx-translate/core';
     MatGridList,
     MatGridTile,
     MatIconModule,
-    TranslatePipe
+    TranslatePipe,
+    TermsAndConditionsComponent
   ],
   templateUrl: './footer-content.component.html',
   styleUrl: './footer-content.component.css'
