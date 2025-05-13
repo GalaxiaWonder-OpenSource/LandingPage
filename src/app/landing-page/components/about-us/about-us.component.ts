b77a5e1bfcbee5de3e427f4a79d6714a288a4699
Henry Reaño <henry29.2004@hotmail.com>
2025-05-12 20:17:05 -0500

    hotfix(tp): fix responsiveness errors in all sections of the landing page

--- src/app/landing-page/components/about-us/about-us.component.ts
+++ src/app/landing-page/components/about-us/about-us.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import {TranslatePipe} from '@ngx-translate/core';
+import {MatGridList, MatGridTile} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-about-us',
-  imports: [MatGridListModule, TranslatePipe],
   templateUrl: './about-us.component.html',
-  styleUrl: './about-us.component.css'
+  imports: [
+    TranslatePipe,
+    MatGridList,
+    MatGridTile
+  ],
+  styleUrls: ['./about-us.component.css']
 })
 export class AboutUsComponent {
+  missionRowHeight = '2:1';
+  valuesRowHeight = '2:1';
 
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver.observe([
+      Breakpoints.HandsetPortrait,
+      Breakpoints.HandsetLandscape
+    ]).subscribe(result => {
+      if (result.matches) {
+        // Ajustar altura en móvil para evitar ruptura visual
+        this.missionRowHeight = '2:2.5';
+        this.valuesRowHeight = '2:2.8';
+      } else {
+        // Mantener altura estándar en escritorio
+        this.missionRowHeight = '2:1';
+        this.valuesRowHeight = '2:1';
+      }
+    });
+  }
 }
