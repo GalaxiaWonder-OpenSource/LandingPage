10c42b3bcd03017ac860c66aa031017b9ceb90b7
L1LZ4Z <joaquinorozco2004@gmail.com>
2025-05-15 04:12:51 -0500

    chore: add web app hyperlink

--- src/app/landing-page/components/call-to-action/call-to-action.component.ts
+++ src/app/landing-page/components/call-to-action/call-to-action.component.ts
@@ -14,7 +14,7 @@ import {MatButton} from '@angular/material/button';
 })
 export class CallToActionComponent {
   @Input() userType!: UserType;
-  actionUrl: string = "https://ashy-sky-06855a61e.6.azurestaticapps.net/";
+  actionUrl: string = "https://delightful-mushroom-0f82ceb1e.6.azurestaticapps.net";
   UserType = UserType;
 
 }
