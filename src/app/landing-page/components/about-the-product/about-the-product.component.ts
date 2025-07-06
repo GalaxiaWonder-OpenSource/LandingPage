77313eeafee55641f192369ea3c83773829ee4ea
CodyLionVivo <leon.fabri.vivas@hotmail.com>
2025-07-05 23:40:34 -0500

    hotfix: fix video of about the product and team

--- src/app/landing-page/components/about-the-product/about-the-product.component.ts
+++ src/app/landing-page/components/about-the-product/about-the-product.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import {PlanItemComponent} from "../plan-item/plan-item.component";
 import {TranslatePipe} from "@ngx-translate/core";
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about-the-product',
-    imports: [
-        TranslatePipe
-    ],
+  imports: [
+    TranslatePipe
+  ],
   templateUrl: './about-the-product.component.html',
   styleUrl: './about-the-product.component.css'
 })
 export class AboutTheProductComponent {
+  videoUrl: SafeResourceUrl;
 
+  constructor(private sanitizer: DomSanitizer) {
+    this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/A0TU4xBPwRY');
+  }
 }
